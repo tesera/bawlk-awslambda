c5508958ba0f07683c5f0f8983122fbd845b088a
Yves Richard <yves.richard@tesera.com>
2015-02-23 08:05:39 -0700

    works as uploaded lambda

--- index.js
+++ index.js
@@ -8,28 +8,45 @@ var AdmZip = require('adm-zip');
 var Q = require('q');
 var bawlk = require('bawlk');
 
-function validate(name, csv, schema) {
+
+function invokeAwk(dataStream, awkArgs) {
     var deferred = Q.defer();
-    var rules = bawlk.getRulesetsFromSchema(schema);
-    var awk = spawn('awk', ['-F', ',', '-f', './node_modules/bawlk/bin/bawlk.awk']);
-    var validator;
-
-    var csvStream = new stream.Readable();
-    csvStream.push(csv);
-    csvStream.push(null);
-
-    rules.pipe(awk.stdin);
+    var awk = spawn('awk', awkArgs);
+    var result = '';
 
     awk.stdout.on('data', function (chunk) {
-        validator+=chunk.toString('utf8');
+        result+=chunk.toString('utf8');
     });
 
     awk.stdout.on('end', function () {
-        var bawlk = spawn('awk', ['-F', ',', '-v', 'FILENAME='+name , '-v', 'action=validate', validator]);
-
-        csvStream.pipe(bawlk.stdin);
-        bawlk.stdout.pipe(process.stdout);
+        console.log() // todo: fix, if remove get EPIPE err!! https://github.com/joyent/node/issues/3211
+        return deferred.resolve(result);
     });
+
+    awk.stdout.on('error', function (err) {
+        return deferred.reject(err);
+    });
+
+    dataStream.pipe(awk.stdin);
+
+    return deferred.promise;
+}
+
+function getBawlkScript(schema) {
+    var rules = bawlk.getRulesetsFromSchema(schema);
+    return invokeAwk(rules, ['-F', ',', '-f', './node_modules/bawlk/bin/bawlk.awk']);
+}
+
+function invokeBawlk(csvStream, filename, action, bawlkScript){
+    return invokeAwk(csvStream, ['-F', ',', '-v', 'FILENAME='+filename , '-v', 'action='+action, bawlkScript]);
+}
+
+function validateCsv(csvStream, filename, bawlkScript) {
+    return invokeBawlk(csvStream, filename, 'validate', bawlkScript);
+}
+
+function importCsv(csvStream, filename, bawlkScript) {
+    return invokeBawlk(csvStream, filename, 'insert', bawlkScript);
 }
 
 exports.handler = function(event, context) {
@@ -38,15 +55,59 @@ exports.handler = function(event, context) {
         Key: event.Records[0].s3.object.key
     };
 
-    s3.getObject(params, function (err, res) {
-        var zip = new AdmZip(res.Body);
-        var datapackage = JSON.parse(zip.getEntry('datapackage.json').getData().toString('utf8'));
+    if (event.Records[0].eventName == 'ObjectCreated:Put' && event.Records[0].s3.object.key.indexOf('datapackage.zip') != -1) {
+        s3.getObject(params, function (err, res) {
+            var zip = new AdmZip(res.Body);
+            var datapackage = JSON.parse(zip.getEntry('datapackage.json').getData().toString('utf8'));
 
-        var validates = _.map(datapackage.resources, function (r) {
-            var schema = r.schema;
-            var csv = zip.getEntry(r.path).getData().toString('utf8');
+            var initResources = _.map(datapackage.resources, function (resource) {
+                var csv = zip.getEntry(resource.path).getData().toString('utf8');
 
-            return validate(r.path, csv, schema); 
+                var csvStream = new stream.Readable();
+                csvStream.push(csv);
+                csvStream.push(null);
+
+                return getBawlkScript(resource.schema).then(function (script) {
+                    return {
+                        filename: resource.path,
+                        csvStream: csvStream,
+                        bawlkScript: script
+                    };
+                });
+            });
+
+            Q.allSettled(initResources)
+                .then(function (results) {
+                    var validates = results.map(function (result) {
+                        if (result.state == 'fulfilled') {
+                            return validateCsv(result.value.csvStream, result.value.filename, result.value.bawlkScript);
+                        } else {
+                            return Q.reject('error valdiating');
+                        }
+                    });
+
+                    return Q.allSettled(validates);
+                })
+                .then(function (results) {
+                    var violations = results.reduce(function (memo, result) {
+                        memo+=result.value;
+                        return memo;
+                    }, '');
+                    params.Body = JSON.stringify(violations);
+                    params.Key = params.Key.replace('datapackage.zip', 'violations.csv');
+
+                    s3.putObject(params, function (err, data) {
+                        if (err) {
+                            console.log(err, err.stack);
+                        }
+                        else {
+                            context.done(null,'');
+                            console.log(data);
+                        }
+                    });
+                });
         });
-    });
+    } else {
+        context.done(null,'');
+    }
 };
--- test.js
+++ test.js
@@ -3,16 +3,23 @@ var lambda = require('./index.js');
 var evt = {
     Records:[
         {
+            eventName: 'ObjectCreated:Put',
             s3: {
                 bucket: {
                     name: 'tesera.datathemes'
                 },
                 object: {
-                    key: 'datapackage.zip'
+                    key: 'afgo.pgyi/uploads/22770144-ec75-4842-ae27-b3ca61545b87/datapackage.zip'
                 }
             }
         }
     ]
 };
 
-lambda.handler(evt);
+var context = {
+    done: function(err, data) {
+        console.log(data);
+    }
+}
+
+lambda.handler(evt, context);
