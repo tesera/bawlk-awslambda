75fb6e2a579c6dfd8c2e7af46f967cbe6e702a32
Yves Richard <yves.richard@tesera.com>
2015-09-14 14:04:49 -0600

    Fix child reporting wrong parent key

--- fkeychecks.sh
+++ fkeychecks.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # checks pgyi dataset for fkey integrity
-# will print out any offending records
+# will print out any offending rows as violation records
 # usage: bash ./fkeycheck.sh <csv_data_folder>
 # example: bash ./fkeycheck.sh ./examples/pgyi/data
-# example output: plot_measurement.csv,n/a,foreign-key,missing referenced parent plot.csv: GOA109,error,2
+# example output: 2,disturbance.csv,COM-123,foreign-key,error,missing parent record in plot.csv
 
-wd=$1
+wd="$1"
 
 plot=plot.csv
 plot_measurement=plot_measurement.csv
@@ -20,19 +20,26 @@ photo_avi=photo_avi.csv
 plot_childs=($plot_measurement $trees $treatment $disturbance $regeneration $photo_avi)
 
 check(){
+    # 1. build array of all parent pkeys
+    # 2. iterate each child and if fkey not in pkeys add to orphans
+    # 3. print each orphan as violation record
+
     awk -F, " \
-        NR == FNR { p=$2; k[$2]=1; next; } \
-        NF { if(!k[$4]) vs[$4] ? vs[$4]++ : vs[$4]=1; } \
-        END { for (v in vs) print NR FS \"$3\" FS p FS \"foreign-key\" FS \"error\" FS \"missing parent record in $1: \" v}" $wd/$1 $wd/$3
+        NR == FNR { pkey=$2; pkeys[pkey]=1; next; } \
+        NF { fkey=$4; if(!pkeys[fkey]) orphans[fkey]=FNR } \
+        END { for (orphan in orphans) print orphans[orphan] FS \"$3\" FS orphan FS \"foreign-key\" FS \"error\" FS \"missing parent record in $1: \" orphan}" "$wd/$1" "$wd/$3"
 }
 
+# echo violation csv record headers
 echo source_row_index,violating_table,violation_key,violation_type,violation_severity,violation_comment
 
+# check all direct childs of plots
 for child in "${plot_childs[@]}"
 do
     check $plot '$1 "-" $2' $child '$1 "-" $2'
 done
 
+# check all other relationships
 check $trees '$1 "-" $2 "-" $3' $trees_measurement '$1 "-" $2 "-" $4'
 check $plot_measurement '$1 "-" $2 "-" $3' $trees_measurement '$1 "-" $2 "-" $3'
 check $plot_measurement '$1 "-" $2 "-" $3' $regeneration '$1 "-" $2 "-" $3'
